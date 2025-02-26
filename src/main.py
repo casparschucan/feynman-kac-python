94148d066b6aa49fec09d12d93f37a22b69fb9d5
Caspar Schucan <caspar.schucan@gmail.com>
2025-02-26 17:11:27 +0100

    first test runs

--- src/main.py
+++ src/main.py
@@ -1,17 +1,30 @@
 from random_walk import feynman_kac_sample
 
-
-def bound(x, y):
-    return 0
+import numpy as np
 
 
-def rhs(x, y):
-    return 1
+def test_phi(x, y):
+    return np.sin(x)*np.sin(y)
 
 
-feynman_kac_sample(1000, 0, 0, bound, rhs)
-print("ran through")
+def feynman_kac_test_sol(x, y, N):
+    expectation = 0
+    Var = 0
 
-sample = feynman_kac_sample(1000, .5, .5, bound, rhs)
+    for i in range(N):
+        sample = feynman_kac_sample(10, x, y, test_phi, test_phi)
+        expectation += sample
+        Var += sample**2
 
-print(sample)
+    expectation /= N
+    Var /= N
+    Var -= expectation**2
+
+    print("found ", expectation,  " with variance ", Var, " expected ",
+          np.sin(y)*np.sin(x))
+    return expectation
+
+
+feynman_kac_test_sol(.5, .5, 4000)
+
+feynman_kac_test_sol(.2, .2, 4000)
--- src/random_walk.py
+++ src/random_walk.py
@@ -1,7 +1,5 @@
 import numpy as np
-import math as m
 
-from numpy.random import Generator
 
 # a function samples a random walk starting from a specific point
 # and returns the needed values for feynman-kac poisson eval on
@@ -12,9 +10,10 @@ def feynman_kac_sample(N: int, x0: float, y0: float, f, g):
 
     x = x0
     y = y0
-    integral = 0.
+    integral = 0
+    total_dist = 0
 
-    delta_x = 0.1
+    delta_x = 0.02
 
     gen = np.random.default_rng()
 
@@ -25,7 +24,10 @@ def feynman_kac_sample(N: int, x0: float, y0: float, f, g):
         eps_y = gen.standard_normal()
         y += eps_y * delta_x
 
-        integral += g(x, y)*(m.sqrt((eps_x*delta_x)**2 + (eps_y*delta_x)**2))
+        dist = np.sqrt((eps_x*delta_x)**2 + (eps_y*delta_x)**2)
+        integral += g(x, y)*dist
+
+        total_dist += dist
 
     if x < 0:
         x = 0
@@ -39,6 +41,6 @@ def feynman_kac_sample(N: int, x0: float, y0: float, f, g):
 
     integral += f(x, y)
 
+    integral /= total_dist
+
     return integral
-
-
