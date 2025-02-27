29f5d532aec61f6d2abb54f9929c9b08e9df4dcc
Caspar Schucan <caspar.schucan@gmail.com>
2025-02-27 14:50:27 +0100

    switch to sin(pi x)sin(pi y)

--- src/main.py
+++ src/main.py
@@ -4,7 +4,15 @@ import numpy as np
 
 
 def test_phi(x, y):
-    return np.sin(x)*np.sin(y)
+    return np.sin(np.pi * x)*np.sin(np.pi * y)
+
+
+def test_bound(x, y):
+    return 0
+
+
+def test_rhs(x, y):
+    return np.pi**2 * (np.sin(np.pi * x)*np.sin(np.pi * y))
 
 
 def feynman_kac_test_sol(x, y, N):
@@ -12,7 +20,10 @@ def feynman_kac_test_sol(x, y, N):
     Var = 0
 
     for i in range(N):
-        sample = feynman_kac_sample(10, x, y, test_phi, test_phi)
+        if i % 100 == 0:
+            print("done with ", i, " iterations")
+
+        sample = feynman_kac_sample(10, x, y, test_bound, test_rhs)
         expectation += sample
         Var += sample**2
 
@@ -21,10 +32,10 @@ def feynman_kac_test_sol(x, y, N):
     Var -= expectation**2
 
     print("found ", expectation,  " with variance ", Var, " expected ",
-          np.sin(y)*np.sin(x))
+          test_phi(x, y))
     return expectation
 
 
-feynman_kac_test_sol(.5, .5, 4000)
+feynman_kac_test_sol(.5, .8, 3000)
 
-feynman_kac_test_sol(.2, .2, 4000)
+feynman_kac_test_sol(.1, .7, 1000)
--- src/random_walk.py
+++ src/random_walk.py
@@ -2,8 +2,7 @@ import numpy as np
 
 
 # a function samples a random walk starting from a specific point
-# and returns the needed values for feynman-kac poisson eval on
-# a 2-D grid discretization of the unit square [0,1]^2
+# and returns the needed values for feynman-kac poisson eval on the unit square
 
 
 def feynman_kac_sample(N: int, x0: float, y0: float, f, g):
@@ -11,23 +10,21 @@ def feynman_kac_sample(N: int, x0: float, y0: float, f, g):
     x = x0
     y = y0
     integral = 0
-    total_dist = 0
+    num_steps = 0
 
-    delta_x = 0.02
+    delta_x = 0.001
 
     gen = np.random.default_rng()
 
     while x > 0 and y > 0 and x < 1 and y < 1:
+        integral += g(x, y)
         eps_x = gen.standard_normal()
         x += eps_x * delta_x
 
         eps_y = gen.standard_normal()
         y += eps_y * delta_x
 
-        dist = np.sqrt((eps_x*delta_x)**2 + (eps_y*delta_x)**2)
-        integral += g(x, y)*dist
-
-        total_dist += dist
+        num_steps += 1
 
     if x < 0:
         x = 0
@@ -39,8 +36,8 @@ def feynman_kac_sample(N: int, x0: float, y0: float, f, g):
     elif y > 1:
         y = 1
 
-    integral += f(x, y)
+    integral /= num_steps
 
-    integral /= total_dist
+    integral += f(x, y)
 
     return integral
