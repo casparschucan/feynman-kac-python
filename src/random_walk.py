7aab69b08d41e7cf4b6d70fa781641d71db4a0f4
Caspar Schucan <caspar.schucan@gmail.com>
2025-03-06 11:03:33 +0100

    MLMC correlated sample

--- src/random_walk.py
+++ src/random_walk.py
@@ -37,3 +37,89 @@ def feynman_kac_sample(x0: float, y0: float, f, g, dt):
     integral += f(x, y)
 
     return integral
+
+
+def is_in_domain(x, y):
+    return x > 0 and y > 0 and x < 1 and y < 1
+
+
+def project_to_domain_edge(x, y):
+    if not is_in_domain(x, y):
+        if x < 0:
+            x = 0
+        elif x > 1:
+            x = 1
+
+        if y < 0:
+            y = 0
+        elif y > 1:
+            y = 1
+    elif x < y:
+        if x < 1-y:
+            x = 0
+        elif y < .5:
+            y = 0
+        else:
+            y = 1
+    else:
+        if y < 1-x:
+            y = 0
+        elif x < 0.5:
+            x = 0
+        else:
+            x = 1
+
+    return x, y
+
+
+def feynman_kac_correlated(x0: float,
+                           y0: float,
+                           f,
+                           g,
+                           dt_fine: float,
+                           level: int):
+    if (level == 0):
+        return feynman_kac_sample(x0, y0, f, g, dt_fine)
+
+    x_fine = x0
+    x_coarse = x0
+    y_fine = y0
+    y_coarse = y0
+
+    coarse_integral = 0
+    fine_integral = 0
+
+    dt_coarse = 2*dt_fine
+
+    gen = np.random.default_rng()
+    while is_in_domain(x_fine, y_fine) and is_in_domain(x_coarse, y_coarse):
+        fine_integral += g(x_fine, y_fine)*dt_fine
+        eps_x1 = gen.normal(scale=np.sqrt(dt_fine))
+        x_fine += eps_x1
+
+        eps_y1 = gen.normal(scale=np.sqrt(dt_fine))
+        y_fine += eps_y1
+
+        if not is_in_domain(x_fine, y_fine):
+            break
+
+        fine_integral += g(x_fine, y_fine)*dt_fine
+        eps_x2 = gen.normal(scale=np.sqrt(dt_fine))
+        x_fine += eps_x2
+
+        eps_y2 = gen.normal(scale=np.sqrt(dt_fine))
+        y_fine += eps_y2
+
+        coarse_integral += g(x_coarse, y_coarse)*dt_coarse
+        eps_x_coarse = (eps_x1 + eps_x2)/np.sqrt(2)
+        x_coarse += eps_x_coarse
+
+        eps_y_coarse = (eps_y1 + eps_y2)/np.sqrt(2)
+        y_coarse += eps_y_coarse
+
+    x_fine, y_fine = project_to_domain_edge(x_fine, y_fine)
+    x_coarse, y_coarse = project_to_domain_edge(x_coarse, y_coarse)
+
+    integral = fine_integral - coarse_integral
+
+    return integral
