96896a5777bbfe063ccda2b15b80f4825328b394
Caspar Schucan <caspar.schucan@gmail.com>
2025-03-18 14:38:43 +0100

    modifications to standard MC eval

--- src/analyze_data.py
+++ src/analyze_data.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 
+def test_phi(x, y):
+    return np.sin(np.pi*x)*np.sin(np.pi*y)
+
+
 def calculate_std(N: int, samples):
     # get standard deviation of the averages of subarrays of length N
     N_tot = len(samples)
@@ -17,12 +21,30 @@ def calculate_std(N: int, samples):
     return np.sqrt(Var)
 
 
+def calculate_mean_err(N: int, samples):
+    mean_err = 0
+
+    N_tot = len(samples)
+
+    if N > N_tot or N_tot % N != 0:
+        print("Oh no that's not good")
+        return 1e10
+
+    for i in range(int(N_tot / N)):
+        start_index: int = int(i*N)
+        end_index: int = int((i+1)*N)
+        mean_err += abs(samples[start_index:end_index].mean() - test_phi(.5, .5))
+
+    mean_err /= N_tot/N
+    return mean_err
+
+
 def check_convergence(samples, Ns):
     errs = np.zeros(len(Ns))
 
     for i in range(len(Ns)):
         N_cur = Ns[i]
 
-        errs[i] = calculate_std(N_cur, samples)
+        errs[i] = calculate_mean_err(N_cur, samples)
 
     return errs
